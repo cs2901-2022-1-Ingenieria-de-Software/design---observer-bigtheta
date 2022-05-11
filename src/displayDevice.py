dd839ab286eca40e62631c7b8e48156b5d547d10
warleon <51892390+warleon@users.noreply.github.com>
2022-05-10 21:41:36 -0500

    o

--- src/displayDevice.py
+++ src/displayDevice.py
@@ -1,16 +1,32 @@
 from observer import Observer
+from sklearn.linear_model import LinearRegression
+from numpy import reshape
+
 
 class Forecast(Observer):
 	def __init__(self):
-		self.future_temperature = 0
-		self.future_humidity = 0
-		self.future_pressure = 0
+		self.past_temperature = []
+		self.past_humidity = []
+		self.past_pressure = []
+		self.xs = []
 		
 	def update(self ,temperature, humidity, pressure):
-		self.future_temperature = temperature
-		self.future_humidity = humidity
-		self.future_pressure = pressure
-		print("Forecast: updated")
+		self.past_temperature.append(temperature)
+		self.past_humidity.append(humidity)
+		self.past_pressure.append(pressure)
+		self.xs.append(len(self.xs))
+		print("Forecast: updated future values", "self.get_future()") 
+
+	"""
+	def get_future(self):
+		ft = LinearRegression()
+		ft.fit(reshape(self.xs,(-1,1)), reshape(self.past_temperature,(-1,1)))
+		fh = LinearRegression()
+		ft.fit(reshape(self.xs,(-1,1)), reshape(self.past_humidity),(-1,1))
+		fp = LinearRegression()
+		ft.fit(reshape(self.xs,(-1,1)), reshape(self.past_pressure),(-1,1))
+		return(ft.predict(len(self.xs)), fh.predict(len(self.xs)), fp.predict(len(self.xs)))
+	"""
 
 class CurrentConditions(Observer):
 	def __init__(self):
@@ -22,16 +38,26 @@ class CurrentConditions(Observer):
 		self.current_temperature = temperature
 		self.current_humidity = humidity
 		self.current_pressure = pressure
-		print("Current_conditions: updated")
+		print("Current_conditions: updated current conditions:",self.get_all())
+
+	def get_all(self):
+		return (self.current_temperature, self.current_humidity, self.current_pressure)
 
 class Statistics(Observer):
 	def __init__(self):
-		self.past_temperature = 0
-		self.past_humidity = 0
-		self.past_pressure = 0
+		self.past_temperature = []
+		self.past_humidity = []
+		self.past_pressure = []
+
+	def mean(self,values):
+		return sum(values)/len(values)
+
+	def all_mean(self):
+		return (self.mean(self.past_temperature),self.mean(self.past_humidity),self.mean(self.past_pressure))
 
 	def update(self ,temperature, humidity, pressure):
-		self.past_temperature = temperature
-		self.past_humidity = humidity
-		self.past_pressure = pressure
-		print("Statistics: updated")
+		self.past_temperature.append(temperature)
+		self.past_humidity.append(humidity)
+		self.past_pressure.append(pressure)
+		print("Statistics: updated mean:",self.all_mean())
+	
